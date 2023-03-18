76dbba08fbdc7ae915a4f302d6bf3aff7c932692
Eraldo dos Santos Neto <eraldo.neto.ext@infracommerce.com.br>
2023-03-18 20:58:07 -0300

    estilização product details

--- components/product/ProductDetails.tsx
+++ components/product/ProductDetails.tsx
@@ -8,12 +8,24 @@ import Icon from "$store/components/ui/Icon.tsx";
 import { useOffer } from "$store/sdk/useOffer.ts";
 import { formatPrice } from "$store/sdk/format.ts";
 import type { LoaderReturnType } from "$live/types.ts";
-import type { ProductDetailsPage } from "deco-sites/std/commerce/types.ts";
+import type {
+  ImageObject,
+  ProductDetailsPage,
+} from "deco-sites/std/commerce/types.ts";
+import Slider from "$store/components/ui/Slider.tsx";
+import SliderControllerJS from "$store/islands/SliderJS.tsx";
+import { useId } from "preact/hooks";
+import { animation, keyframes, tw } from "twind/css";
 
 import ProductSelector from "./ProductVariantSelector.tsx";
 
 export interface Props {
   page: LoaderReturnType<ProductDetailsPage | null>;
+}
+
+interface Dots {
+  interval?: number;
+  imagesProduct?: [ImageObject];
 }
 
 function NotFound() {
@@ -29,7 +41,91 @@ function NotFound() {
   );
 }
 
-function Details({ page }: { page: ProductDetailsPage }) {
+function Dots({ imagesProduct, interval = 0 }: Dots) {
+  return (
+    <>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+          @property --dot-progress {
+            syntax: '<percentage>';
+            inherits: false;
+            initial-value: 0%;
+          }`,
+        }}
+      >
+      </style>
+      <ol class="flex items-center justify-center col-span-full gap-4 z-10 row-start-4 lg:flex-col lg:z-0 lg:justify-start lg:mt-[30px]">
+        {imagesProduct?.map((_: ImageObject, index: number) => (
+          <li class="h-full lg:h-auto">
+            <button
+              data-dot={index}
+              aria-label={`go to slider item ${index}`}
+              class="h-full rounded focus:outline-none group"
+            >
+              <div
+                class={tw`w-16 h-2 w-2 rounded-full bg-black block lg:hidden`}
+              />
+              <Image
+                style={{ aspectRatio: "360 / 500" }}
+                class="snap-center min-w-[84px] hidden lg:block"
+                sizes="(max-width: 84px) 100vw, 30vw"
+                src={_.url!}
+                alt={_.alternateName}
+                width={84}
+                height={125}
+                // Preload LCP image for better web vitals
+                preload={index === 0}
+                loading={index === 0 ? "eager" : "lazy"}
+              />
+            </button>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
+}
+
+function Controls() {
+  return (
+    <>
+      <div class="items-center justify-center z-10 absolute top-1/2 left-[15px] md:left-[5%] -translate-y-[50%] hidden">
+        <Button
+          class="h-[60px] w-[60px] p-[10px] bg-bgArrow border-1 border-default"
+          variant="iconArrow"
+          data-slide="prev"
+          aria-label="Previous item"
+        >
+          <Icon
+            class="text-default-inverse"
+            size={40}
+            id="ChevronLeft"
+            strokeWidth={2}
+          />
+        </Button>
+      </div>
+      <div class="items-center justify-center z-10 absolute top-1/2 right-[15px] md:right-[5%] -translate-y-[50%] hidden">
+        <Button
+          class="h-[60px] w-[60px] p-[10px] bg-bgArrow border-1 border-default"
+          variant="iconArrow"
+          data-slide="next"
+          aria-label="Next item"
+        >
+          <Icon
+            class="text-default-inverse"
+            size={40}
+            id="ChevronRight"
+            strokeWidth={2}
+          />
+        </Button>
+      </div>
+    </>
+  );
+}
+
+function Details(
+  { page, interval = 3 }: { page: ProductDetailsPage; interval: number },
+) {
   const {
     breadcrumbList,
     product,
@@ -41,92 +137,153 @@ function Details({ page }: { page: ProductDetailsPage }) {
     image: images,
     name,
     gtin,
+    isVariantOf,
   } = product;
   const { price, listPrice, seller, installments } = useOffer(offers);
   const [front, back] = images ?? [];
+  const id = useId() + 2;
 
   return (
     <Container class="py-0 sm:py-10">
-      <div class="flex flex-col gap-4 sm:flex-row sm:gap-10">
+      <div
+        class="flex flex-col gap-4 mt-[98px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px] 2xl:max-w-[1320px] mx-auto"
+        id={id}
+      >
+        {/* Breadcrumb */}
+        <Breadcrumb
+          itemListElement={breadcrumbList?.itemListElement.slice(0, -1)}
+        />
         {/* Image Gallery */}
-        <div class="flex flex-row overflow-auto snap-x snap-mandatory scroll-smooth sm:gap-2">
-          {[front, back ?? front].map((img, index) => (
-            <Image
-              style={{ aspectRatio: "360 / 500" }}
-              class="snap-center min-w-[100vw] sm:min-w-0 sm:w-auto sm:h-[600px]"
-              sizes="(max-width: 640px) 100vw, 30vw"
-              src={img.url!}
-              alt={img.alternateName}
-              width={360}
-              height={500}
-              // Preload LCP image for better web vitals
-              preload={index === 0}
-              loading={index === 0 ? "eager" : "lazy"}
+        <div class="lg:flex lg:justify-between lg:gap-[8%]">
+          <div class="lg:flex lg:flex-row-reverse lg:justify-between lg:gap-[15px]">
+            <Slider class="col-span-full row-span-full scrollbar-none gap-6 lg:max-w-[380px] xl:max-w-[480px] 2xl:max-w-[550px] 2xl:mt-[30px]">
+              {[front, back ?? front].map((img, index) => (
+                <Image
+                  style={{ aspectRatio: "360 / 500" }}
+                  class="snap-center min-w-[100vw] lg:min-w-[380px] xl:min-w-[480px] 2xl:min-w-[550px]"
+                  sizes="(max-width: 640px) 100vw, 30vw"
+                  src={img.url!}
+                  alt={img.alternateName}
+                  width={360}
+                  height={500}
+                  // Preload LCP image for better web vitals
+                  preload={index === 0}
+                  loading={index === 0 ? "eager" : "lazy"}
+                />
+              ))}
+            </Slider>
+            <Dots imagesProduct={front} interval={interval} />
+            <Controls />
+            <SliderControllerJS
+              rootId={id}
+              interval={interval && interval * 1e3}
             />
-          ))}
-        </div>
-        {/* Product Info */}
-        <div class="flex-auto px-4 sm:px-0">
-          {/* Breadcrumb */}
-          <Breadcrumb
-            itemListElement={breadcrumbList?.itemListElement.slice(0, -1)}
-          />
-          {/* Code and name */}
-          <div class="mt-4 sm:mt-8">
-            <div>
-              <Text tone="subdued" variant="caption">
-                Cod. {gtin}
-              </Text>
-            </div>
-            <h1>
-              <Text variant="heading-3">{name}</Text>
-            </h1>
           </div>
-          {/* Prices */}
-          <div class="mt-4">
-            <div class="flex flex-row gap-2 items-center">
+          {/* Product Info */}
+          <div class="flex-auto px-4 sm:px-0 lg:max-w-[400px] xl:max-w-[500px] lg:mt-[30px]">
+            {/* Code and name */}
+            <div class="mt-4 lg:mt-0">
+              <h1>
+                <Text variant="heading-3">{isVariantOf?.name}</Text>
+              </h1>
+              <div>
+                <Text tone="subdued" variant="caption">
+                  Código: {gtin}
+                </Text>
+              </div>
+            </div>
+            {/* Prices */}
+            <div class="mt-4">
               <Text
-                class="line-through"
+                class="line-through block"
                 tone="subdued"
                 variant="list-price"
               >
                 {formatPrice(listPrice, offers!.priceCurrency!)}
               </Text>
-              <Text tone="price" variant="heading-3">
+              <Text class="block text-badge" tone="price" variant="heading-3">
                 {formatPrice(price, offers!.priceCurrency!)}
               </Text>
+              <Text class="block" tone="subdued" variant="caption">
+                ou {installments}
+              </Text>
             </div>
-            <Text tone="subdued" variant="caption">
-              {installments}
-            </Text>
-          </div>
-          {/* Sku Selector */}
-          <div class="mt-4 sm:mt-6">
-            <ProductSelector product={product} />
-          </div>
-          {/* Add to Cart and Favorites button */}
-          <div class="mt-4 sm:mt-10 flex flex-col gap-2">
-            {seller && (
-              <AddToCartButton
-                skuId={productID}
-                sellerId={seller}
-              />
-            )}
-            <Button variant="secondary">
-              <Icon id="Heart" width={20} height={20} strokeWidth={2} />{" "}
-              Favoritar
-            </Button>
-          </div>
-          {/* Description card */}
-          <div class="mt-4 sm:mt-6">
-            <Text variant="caption">
-              {description && (
-                <details>
-                  <summary class="cursor-pointer">Descrição</summary>
-                  <div class="ml-2 mt-2">{description}</div>
-                </details>
+            {/* Sku Selector */}
+            <div class="mt-4 sm:mt-6 lg:flex lg:justify-between lg:items-center">
+              <ProductSelector product={product} />
+              <div class="mt-4 flex justify-content items-center">
+                <Icon class="mr-[10px]" id="Meansure" width={28} height={22} />
+                <span class="text-uppercase text-[0.75rem] text-[#909090]">
+                  Guia de medidas
+                </span>
+              </div>
+            </div>
+            {/* Add to Cart and Favorites button */}
+            <div class="fixed bottom-0 left-0 min-w-full bg-default shadow-md lg:relative lg:shadow-none">
+              <div class="flex justify-between items-center px-2 py-4">
+                <Text
+                  class="block text-badge lg:hidden"
+                  tone="price"
+                  variant="small-text"
+                >
+                  {formatPrice(price, offers!.priceCurrency!)}
+                </Text>
+                <Text
+                  class="block lg:hidden"
+                  tone="subdued"
+                  variant="small-text"
+                >
+                  ou {installments}
+                </Text>
+              </div>
+              {seller && (
+                <AddToCartButton
+                  skuId={productID}
+                  sellerId={seller}
+                />
               )}
-            </Text>
+            </div>
+            {/* Consulte prazo de entrega */}
+            <div class="pt-4">
+              <Text class="text-uppercase">
+                Consulte prazo de entrega e frete
+              </Text>
+              <span class="text-[12px] hidden md:block mt-[12px]">
+                Insira seu CEP
+              </span>
+              <div class="block md:flex justify-center items-center md:gap-6 md:justify-between lg:flex-wrap lg:gap-x-6 lg:gap-y-0">
+                <input
+                  class="md:max-w-[320px] border-b-[2px] mt-2 border-frete w-full bg-default tracking-[2px] text-uppercase pt-4 lg:max-w-[200px] xl:max-w-[280px]"
+                  type="tel"
+                />
+                <Button
+                  variant="tertiary"
+                  class="md:max-w-[135px] border-[1px] border-black bg-transparent mt-2 border-frete w-full bg-default tracking-[2px] text-uppercase py-6 text-default"
+                >
+                  Calcular
+                </Button>
+                <a
+                  class="text-[10px] text-underline font-strong md:max-w-[85px] lg:block"
+                  href="https://buscacepinter.correios.com.br/app/endereco/index.php"
+                >
+                  Não sei meu CEP
+                </a>
+              </div>
+            </div>
+
+            {/* Description card */}
+            <div class="mt-10 mb-20 md:mt-20">
+              <Text variant="caption">
+                {description && (
+                  <details>
+                    <summary class="cursor-pointer text-uppercase pb-2 border-b-1 border-black">
+                      Sobre essa peça
+                    </summary>
+                    <div class="ml-2 mt-2">{description}</div>
+                  </details>
+                )}
+              </Text>
+            </div>
           </div>
         </div>
       </div>
@@ -136,7 +293,7 @@ function Details({ page }: { page: ProductDetailsPage }) {
 
 function ProductDetails({ page }: Props) {
   if (page) {
-    return <Details page={page} />;
+    return <Details page={page} interval={3} />;
   }
 
   return <NotFound />;
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -18,11 +18,13 @@ const variants = {
   secondary:
     "h-[36px] px-3 bg-interactive-inverse font-button text-button border-default hover:bg-hover active:bg-interactive active:text-interactive-inverse disabled:border-default disabled:text-subdued disabled:bg-interactive-inverse focus:outline-none",
   tertiary:
-    "h-[36px] px-3 bg-interactive-inverse font-button text-button border-transparent hover:bg-hover active:border-interactive disabled:border-transparent disabled:text-subdued focus:outline-none",
+    "h-[36px] px-3 bg-interactive-inverse font-button text-button hover:bg-hover active:border-interactive disabled:border-transparent disabled:text-subdued focus:outline-none",
   icon:
     "rounded-full bg-transparent text-default border-none disabled:text-subdued disabled:bg-interactive-inverse focus:outline-none",
   iconArrow:
     "rounded-sm bg-transparent text-default disabled:text-subdued disabled:bg-interactive-inverse focus:outline-none",
+  buy:
+    "h-[36px] px-3 bg-badge font-button text-button text-default-inverse border-transparent hover:opacity-75 transition duration-300 active:bg-interactive active:text-default-inverse active:border-transparent disabled:border-default disabled:text-subdued disabled:bg-interactive-inverse focus:outline-none ",
 };
 
 const Button = forwardRef<HTMLButtonElement, Props>(({
