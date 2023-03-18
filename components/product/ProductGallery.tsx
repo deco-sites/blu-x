f1c8b837d8ece006f06923d4fae50073c444b67a
Fausto Mastrella <fausto.mastrella@infracommerce.com.br>
2023-03-18 15:01:29 -0300

    Shelf novidades

--- components/product/ProductGallery.tsx
+++ components/product/ProductGallery.tsx
@@ -19,6 +19,7 @@ function NotFound() {
 }
 
 function Gallery({ page }: { page: ProductListingPage }) {
+
   return (
     <Container class="px-4 sm:py-10">
       <div class="relative grid grid-cols-2 sm:grid-cols-4 gap-2 sm:gap-10 items-center">
