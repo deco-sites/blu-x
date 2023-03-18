aebd66eb14c694a1369367121c252f4b318ef9ec
Erick Cordeiro <erickcordeiroa@gmail.com>
2023-03-18 09:30:30 -0300

    Draft: Alter footer responsive

--- components/footer/Newsletter.tsx
+++ components/footer/Newsletter.tsx
@@ -8,12 +8,12 @@ function Newsletter() {
           variant="heading-2"
           tone="default-inverse"
           class="font-newsletter"
-        >
+        > 
           <strong class="font-semibold">CADASTRE-SE</strong>{" "}
           E RECEBA NOVIDADES E PROMOÇÕES
         </Text>
       </div>
-      <form class="flex flex-row items-center gap-4 font-body text-body w-full sm:w-[458px]">
+      <form class="flex flex-col md:flex-row items-center gap-4 font-body text-body w-full sm:w-[458px]">
         <input
           class="h-12 px-3 flex-grow bg-footer text-[#dee2e6] border-1 border-[#dee2e6]"
           placeholder="Nome"
@@ -23,7 +23,7 @@ function Newsletter() {
           placeholder="E-mail"
         />
         <button
-          class="h-12 text-sm text-white px-3 bg-badge"
+          class="h-12 px-10 text-sm text-white bg-badge min-w-max font-bold"
           type="bgutton" // prevent form's default behavior
         >
           Cadastrar
