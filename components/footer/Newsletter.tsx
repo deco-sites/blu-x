3e112b84c5d66a825431069ecd527157245cd83f
Fausto Mastrella <fausto.mastrella@infracommerce.com.br>
2023-03-19 09:13:14 -0300

    Style fix for newsletter

--- components/footer/Newsletter.tsx
+++ components/footer/Newsletter.tsx
@@ -1,25 +1,26 @@
 import Text from "$store/components/ui/Text.tsx";
+import ContainerCustom from "../ui/ContainerCustom.tsx";
 
 function Newsletter() {
   return (
-    <div class="flex flex-col sm:flex-row items-center gap-6 sm:gap-20">
-      <div class="flex flex-col gap-2 max-w-[350px]">
+    <div class="flex flex-col px-4 md:flex-row items-center gap-6 sm:gap-10">
+      <div class="max-w-[350px] text-center md:text-left">
         <Text
-          variant="heading-2"
-          tone="default-inverse"
-          class="font-newsletter"
-        >
-          <strong class="font-semibold">CADASTRE-SE</strong>{" "}
-          E RECEBA NOVIDADES E PROMOÇÕES
-        </Text>
+            variant="heading-2"
+            tone="default-inverse"
+            class="uppercase"
+          >
+            <strong class="font-semibold">Cadastre-se</strong>{" "}
+            e receba novidades e promoções 
+          </Text>
       </div>
-      <form class="flex flex-col md:flex-row items-center gap-4 font-body text-body w-full sm:w-[458px]">
+      <form class="flex flex-col md:flex-row items-center gap-4 font-body text-body sm:w-[458px]" style={{width:'100%'}}>
         <input
-          class="h-12 px-3 flex-grow bg-footer text-[#dee2e6] border-1 border-[#dee2e6]"
+          class="h-12 w-full px-3 flex-grow bg-footer text-[#dee2e6] border-1 border-[#dee2e6]"
           placeholder="Nome"
         />
         <input
-          class="h-12 px-3 flex-grow bg-footer text-[#dee2e6] border-1 border-[#dee2e6]"
+          class="h-12 w-full px-3 flex-grow bg-footer text-[#dee2e6] border-1 border-[#dee2e6]"
           placeholder="E-mail"
         />
         <button
