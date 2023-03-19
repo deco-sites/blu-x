879fbc93dd7b0efed54b1568e148ae43fe2da3a0
Eraldo dos Santos Neto <eraldo.neto.ext@infracommerce.com.br>
2023-03-19 10:15:02 -0300

    footer admin

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,9 +1,14 @@
 export { default } from "$store/components/footer/Footer.tsx";
 export type {
+  Atendimento,
+  Certificados,
+  Copyright,
   IconItem,
   Item,
   Props,
+  RedesSociais,
   Section,
   StringItem,
 } from "$store/components/footer/Footer.tsx";
 export type { AvailableIcons } from "$store/components/ui/Icon.tsx";
+export type { Image as LiveImage } from "deco-sites/std/components/types.ts";
